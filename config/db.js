09d721907805933eaad33fedd81f2aa6fca0c6ea
Lilian Acom <acomlilian2000@gmail.com>
2026-05-16 11:17:25 +0300

    update

--- config/db.js
+++ config/db.js
@@ -1,6 +1,8 @@
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
+   console.log(process.env.DATABASE);
+
   try {
     const conn = await mongoose.connect(process.env.DATABASE);
     console.log(" congs mongoose has successfully opened");
